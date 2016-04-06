37f8a782de2d27acbd78c3db504b6350a96b784a
Steve Zhang <stevzhg@gmail.com>
2016-04-06 19:04:38 +0800

    modify base activity and main activity

---
+++ app/src/main/java/com/steve/wanqureader/presentation/ui/activities/BaseActivity.java
@@ -0,0 +1,112 @@
+package com.steve.wanqureader.presentation.ui.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.steve.wanqureader.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.realm.Realm;
+
+/**
+ * Created by steve on 3/8/16.
+ */
+public abstract class BaseActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private Realm realm;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // set up layout
+        setContentView(R.layout.activity_base);
+        ButterKnife.bind(this);
+        // calling Realm.getDefaultInstance() multiple times on the same thread is free
+        realm = Realm.getDefaultInstance();
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        init(savedInstanceState);
+    }
+
+    protected abstract void init(Bundle savedInstanceState);
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);//解除绑定，官方文档只对fragment做了解绑
+        realm.close();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+
+        } else if (id == R.id.nav_archive) {
+
+        } else if (id == R.id.nav_like) {
+
+        } else if (id == R.id.nav_info) {
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/steve/wanqureader/presentation/ui/activities/MainActivity.java
@@ -0,0 +1,120 @@
+package com.steve.wanqureader.presentation.ui.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.steve.wanqureader.R;
+import com.steve.wanqureader.domain.executor.impl.ThreadExecutor;
+import com.steve.wanqureader.network.model.Post;
+import com.steve.wanqureader.presentation.CustomTabActivityHelper;
+import com.steve.wanqureader.presentation.WebviewFallback;
+import com.steve.wanqureader.presentation.presenters.MainPresenter;
+import com.steve.wanqureader.presentation.presenters.impl.MainPresenterImpl;
+import com.steve.wanqureader.presentation.ui.adapters.PostsAdapter;
+import com.steve.wanqureader.storage.PostRepositoryImpl;
+import com.steve.wanqureader.threading.MainThreadImpl;
+import com.steve.wanqureader.utils.SnackbarUtil;
+
+import java.util.List;
+
+public class MainActivity extends BaseActivity
+        implements MainPresenter.View, SwipeRefreshLayout.OnRefreshListener {
+    private PostsAdapter mAdapter;
+    private MainPresenter mMainPresenter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerView;
+    private View containerView;
+
+    public static void actionStart(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+        containerView = View.inflate(this, R.layout.view_common_list, null);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) containerView.findViewById(R.id.swipe);
+        mRecyclerView = (RecyclerView) containerView.findViewById(R.id.list);
+
+        FrameLayout parentsView = (FrameLayout) findViewById(R.id.frame_layout);
+        if (parentsView == null) return;
+        parentsView.addView(containerView);
+
+        mSwipeRefreshLayout.setColorSchemeResources(
+                R.color.blue700,
+                R.color.green700,
+                R.color.red700,
+                R.color.orange700
+        );
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        mAdapter = new PostsAdapter(this, this);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // allows for optimizations if all items are of the same size
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        // instantiate the presenter
+        mMainPresenter = new MainPresenterImpl(
+                ThreadExecutor.getInstance(),
+                MainThreadImpl.getInstance(),
+                this,
+                new PostRepositoryImpl(this)
+        );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMainPresenter.resume();
+    }
+
+    @Override
+    public void onRefresh() {
+        mMainPresenter.getLatestPosts();
+    }
+
+    @Override
+    public void showPosts(List<Post> posts) {
+        mAdapter.refreshPosts(posts);
+    }
+
+    @Override
+    public void onClickReadPost(Post post) {
+        CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder().build();
+        CustomTabActivityHelper.openCustomTab(this, customTabsIntent,
+                Uri.parse(post.getUrl()), new WebviewFallback());
+    }
+
+    @Override
+    public void onClickLikePost(Post post) {
+
+    }
+
+    @Override
+    public void onPostLiked(Post post) {
+
+    }
+
+    @Override
+    public void onSetProgressBarVisibility(boolean visibility) {
+        mSwipeRefreshLayout.setRefreshing(visibility);
+    }
+
+    @Override
+    public void onError(String message) {
+        SnackbarUtil.show(containerView, message, 0);
+    }
+}
