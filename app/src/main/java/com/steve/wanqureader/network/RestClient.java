01fc863a15cd2b806cb2dc33b291fd1801b99225
Steve Zhang <stevzhg@gmail.com>
2016-04-08 20:16:05 +0800

    add http cache

--- app/src/main/java/com/steve/wanqureader/network/RestClient.java
+++ app/src/main/java/com/steve/wanqureader/network/RestClient.java
@@ -1,8 +1,15 @@
 package com.steve.wanqureader.network;
 
+import com.steve.wanqureader.WanquApplication;
 import com.steve.wanqureader.utils.Constant;
 
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Cache;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,16 +24,33 @@ public class RestClient {
 
     static {
         // enable logging
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        final HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
+        final Interceptor cacheControlInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Response originalResponse = chain.proceed(chain.request());
+                int maxAge = Constant.CACHE_TIME;
+                return originalResponse.newBuilder()
+                        .header("Cache-Control", "public, max-age=" + maxAge)
+                        .build();
+            }
+        };
+
+        //setup cache
+        File httpCacheDirectory = new File(WanquApplication.getContext().getCacheDir(), "responses");
+        Cache cache = new Cache(httpCacheDirectory, Constant.CACHE_SIZE);
+
         OkHttpClient client = new OkHttpClient.Builder()
+                .addNetworkInterceptor(cacheControlInterceptor)
                 .addInterceptor(interceptor)
 //                .addNetworkInterceptor(new StethoInterceptor())
+                .cache(cache) //add cache to the client
                 .build();
 
         mRetrofit = new Retrofit.Builder()
-                .baseUrl(Constant.baseUrl)
+                .baseUrl(Constant.BASE_API_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
